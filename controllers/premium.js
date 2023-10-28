60c622059d9972f7447003a3c201f2c911d6ecf0
Aruns96 <arun509577@gmail.com>
2023-10-29 00:16:12 +0530

    leadboard opt

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,26 +1,35 @@
 const User = require("../models/user");
 const Expense = require("../models/expense");
+const sequelize = require("../utils/database");
 
 
 const getUserLeaderBoard = async(req,res)=>{
     try{
-       const users = await User.findAll();
-       const expenses = await Expense.findAll();
-       const userAggExp = {};
-       expenses.forEach(expense=>{
-        if(userAggExp[expense.userId]){
-            userAggExp[expense.userId] = Number(userAggExp[expense.userId]) + Number(expense.amount) ;
-        }else{
-            userAggExp[expense.userId] = expense.amount;
-        }
-       })
-
-       var userLeadBoardDetails = [];
-
-       users.forEach(user=>{
-        userLeadBoardDetails.push({name:user.name,totol_cost:userAggExp[user.id]||0})
+       const userLeadBoardDetails = await User.findAll({
+        attributes:['id','name',[sequelize.fn('sum',sequelize.col('expenses.amount')),'totol_cost']],
+        include:[{
+            model:Expense,
+            attributes:[]
+        }],
+        group:['user.id'],
+        order:[['totol_cost','DESC']]
        });
-       userLeadBoardDetails.sort((a,b)=>b.totol_cost-a.totol_cost);
+    //    const expenses = await Expense.findAll();
+    //    const userAggExp = {};
+    //    expenses.forEach(expense=>{
+    //     if(userAggExp[expense.userId]){
+    //         userAggExp[expense.userId] = Number(userAggExp[expense.userId]) + Number(expense.amount) ;
+    //     }else{
+    //         userAggExp[expense.userId] = expense.amount;
+    //     }
+    //    })
+
+    //    var userLeadBoardDetails = [];
+
+    //    users.forEach(user=>{
+    //     userLeadBoardDetails.push({name:user.name,totol_cost:userAggExp[user.id]||0})
+    //    });
+    //    userLeadBoardDetails.sort((a,b)=>b.totol_cost-a.totol_cost);
        //console.log(userLeadBoardDetails)
        res.status(200).json(userLeadBoardDetails)
  
